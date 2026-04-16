a09295289a70160d338ec676cc28185030087027
Stace Worrell <sworrel2@utk.edu>
2026-04-16 12:54:00 -0400

    added man2plain.sh with utf8

--- dist/man2plain.sh
+++ dist/man2plain.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while IFS= read -r cmd; do
-pandoc --from man --to plain "$(man -w ${cmd})" | col -b >"$cmd".txt 2>error.err
+pandoc --from man --to plain "$(man -w -Tutf8 ${cmd})" | col -b >"$cmd".txt 2>error.err
 done < <(compgen -c | sort | uniq)
 find . -type f -size 1c -delete
