48aadf70258efd43429b6a594c03de633fd4e62f
Stace Worrell <sworrel2@utk.edu>
2026-04-16 13:28:58 -0400

    updated for man2plain

--- dist/man2plain.sh
+++ dist/man2plain.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 while IFS= read -r cmd; do
-man -E utf8 "$cmd" 2>/dev/null | col -b | pandoc --from man --to plain >"$cmd".txt 2>error.err
-#pandoc --from man --to plain "$(man -Tutf8 ${cmd})" | col -b >"$cmd".txt 2>error.err
+   man -E utf8 "$cmd" 2>/dev/null | col -b | pandoc --from man --to plain >"$cmd".txt 2>error.err
 done < <(compgen -c | sort | uniq)
 find . -type f -size 1c -delete
