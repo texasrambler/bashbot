15c57aa7c3dad8304a64349d0e49bb67d3ee3d45
Stace Worrell <sworrel2@utk.edu>
2026-04-16 13:19:47 -0400

    Added encoding to man

--- dist/man2plain.sh
+++ dist/man2plain.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 while IFS= read -r cmd; do
-pandoc --from man --to plain "$(man -w -Tutf8 ${cmd})" | col -b >"$cmd".txt 2>error.err
+man -E utf8 "$cmd" 2>/dev/null | col -b | pandoc --from man --to plain >"$cmd".txt 2>error.err
+#pandoc --from man --to plain "$(man -Tutf8 ${cmd})" | col -b >"$cmd".txt 2>error.err
 done < <(compgen -c | sort | uniq)
 find . -type f -size 1c -delete
